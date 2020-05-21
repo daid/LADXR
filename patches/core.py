2a8886a9f481ab6b6a72076ef1b0873f7c37c34b
daid <daid303@gmail.com>
2020-05-21 16:03:08 +0200

    License, tiny cleanup

--- patches/core.py
+++ patches/core.py
@@ -5,22 +5,22 @@ from roomEditor import RoomEditor
 def noSwordMusic(rom):
     # Skip no-sword music override
     # Instead of loading the sword level, we put the value 1 in the A register, indicating we have a sword.
-    rom.patch(2, 0x0151, ASM("ld a, [$DB4E]"), ASM("ld a, $01\nNOP"))
-    rom.patch(2, 0x3B42, ASM("ld a, [$DB4E]"), ASM("ld a, $01\nNOP"))
-    rom.patch(3, 0x0994, ASM("ld a, [$DB4E]"), ASM("ld a, $01\nNOP"))
+    rom.patch(2, 0x0151, ASM("ld a, [$DB4E]"), ASM("ld a, $01"), fill_nop=True)
+    rom.patch(2, 0x3B42, ASM("ld a, [$DB4E]"), ASM("ld a, $01"), fill_nop=True)
+    rom.patch(3, 0x0994, ASM("ld a, [$DB4E]"), ASM("ld a, $01"), fill_nop=True)
 
 def removeGhost(rom):
     ## Ghost patch
     # Do not have the ghost follow you after dungeon 4
-    rom.patch(0x03, 0x1E0A, ASM("LD [$DB79], A"), ASM("NOP\nNOP\nNOP"))
+    rom.patch(0x03, 0x1E0A, ASM("LD [$DB79], A"), "", fill_nop=True)
 
 def alwaysAllowSecretBook(rom):
     rom.patch(0x15, 0x3F25, ASM("ld a, [$DB0E]\ncp $0E"), ASM("xor a\ncp $00"), fill_nop=True)
 
 def flameThrowerShieldRequirement(rom):
     rom.patch(0x03, 0x2EAF,
-        ASM("ld a, [$DB44]\ncp $02\nret nz"), # if not shield level 2
-        ASM("ld a, [$DB44]\ncp $02\nret c")) # if not shield level 2 or higher
+        ASM("ld a, [$DB44]\ncp $02\nret nz"),  # if not shield level 2
+        ASM("ld a, [$DB44]\ncp $02\nret c"))  # if not shield level 2 or higher
 
 def cleanup(rom):
     # Remove unused rooms to make some space in the rom
