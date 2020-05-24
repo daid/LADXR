6a014e89b2b8c458b2243bf8877c6f8d31b0d5e9
daid <daid303@gmail.com>
2020-05-24 07:02:07 +0200

    Fix dungeon 8 key logic

--- logic/dungeon8.py
+++ logic/dungeon8.py
@@ -20,7 +20,8 @@ upper_center = Location(8).connect(lower_center, KEY8)
 upper_center.add(DroppedKey(0x23E))
 middle_center_1 = Location(8).connect(upper_center, BOMB)
 middle_center_2 = Location(8).connect(middle_center_1, KEY8)
-miniboss = Location(8).connect(middle_center_2, KEY8)
+middle_center_3 = Location(8).connect(middle_center_2, KEY8)
+miniboss = Location(8).connect(middle_center_3, KEY8)
 miniboss.add(DungeonChest(0x237))
 
 up_left = Location(8).connect(upper_center, KEY8)
@@ -30,3 +31,6 @@ Location(8).add(DroppedKey(0x241)).connect(up_left, BOW)
 Location(8).add(DungeonChest(0x23A)).connect(up_left, HOOKSHOT)
 
 nightmare_key = Location(8).add(DungeonChest(0x232)).connect(entrance_up, MAGIC_ROD)
+
+# Bombing from the center dark rooms to the left so you can access more keys.
+middle_center_2.connect(up_left, AND(BOMB, FEATHER), one_way=True)
