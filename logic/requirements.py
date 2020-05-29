b3f093acc710c1867c8aa100784b4e9dd2f387b8
dragonc0 <66092539+dragonc0@users.noreply.github.com>
2020-05-29 20:12:34 +0200

    Update requirements shortcuts
    Add hookshot and powder shortcuts
    Add hookshot to kill skeleton
    Add range rear attack options for shyguys d2

--- logic/requirements.py
+++ logic/requirements.py
@@ -44,7 +44,10 @@ def isConsumable(item):
 
 bush = OR(SWORD, MAGIC_POWDER, MAGIC_ROD, POWER_BRACELET)
 attack = OR(SWORD, BOMB, BOW, MAGIC_ROD, BOOMERANG)
-attack_no_bomb = OR(SWORD, BOW, MAGIC_ROD, BOOMERANG)
-attack_skeleton = OR(SWORD, BOMB, BOW, BOOMERANG)  # cannot kill skeletons with the fire rod
+attack_hookshot = OR(SWORD, BOMB, BOW, MAGIC_ROD, BOOMERANG, HOOKSHOT)
+attack_powder = OR(SWORD, BOMB, BOW, MAGIC_ROD, BOOMERANG, HOOKSHOT, POWDER)
+attack_no_bomb = OR(SWORD, BOW, MAGIC_ROD, BOOMERANG, HOOKSHOT)
+attack_skeleton = OR(SWORD, BOMB, BOW, BOOMERANG, HOOKSHOT)  # cannot kill skeletons with the fire rod
 rear_attack = OR(SWORD, BOMB)
+rear_attack_range = OR(MAGIC_ROD, BOW)
 fire = OR(MAGIC_POWDER, MAGIC_ROD)
