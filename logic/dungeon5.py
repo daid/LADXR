923ea26ecda0765174ab13a28bb5ccd0478226d2
dragonc0 <66092539+dragonc0@users.noreply.github.com>
2020-05-29 22:05:29 +0200

    Logic changes for d5
    Use new attack_hookshot requirement
    Split area2 from chests for attack requirements
    split 50 rupee chest before bridge from other two to add boots jump to chest into logic

--- logic/dungeon5.py
+++ logic/dungeon5.py
@@ -9,13 +9,15 @@ class Dungeon5:
     def __init__(self):
         entrance = Location(5)
         Location(5).add(DungeonChest(0x1A0)).connect(entrance, HOOKSHOT)
-        compass = Location(5).add(DungeonChest(0x19E)).connect(entrance, OR(attack, HOOKSHOT))
+        compass = Location(5).add(DungeonChest(0x19E)).connect(entrance, attack_hookshot)
         Location(5).add(DroppedKey(0x181)).connect(compass, AND(SWORD, FEATHER))
 
-        area2 = Location(5).add(DungeonChest(0x19B)).connect(entrance, KEY5)  # map chest
+        area2 = Location(5).connect(entrance, KEY5)  
+        Location(5).add(DungeonChest(0x19B)).connect(area2, attack_hookshot)  # map chest
         Location(5).add(DungeonChest(0x197)).connect(area2, HOOKSHOT)  # key chest on the left
-        after_stalfos = Location(5).add(DungeonChest(0x196)).connect(area2, AND(SWORD, BOMB)) # Need to defeat master stalfos once for this empty chest
-        Location(5).add(DungeonChest(0x18E), DungeonChest(0x188), DungeonChest(0x18F)).connect(after_stalfos, AND(FEATHER, HOOKSHOT))
+        after_stalfos = Location(5).add(DungeonChest(0x196)).connect(area2, AND(SWORD, BOMB)) # Need to defeat master stalfos once for this empty chest; l2 sword beams kill but obscure
+        Location(5).add(DungeonChest(0x18E)).connect(after_stalfos, AND(FEATHER, OR(PEGASUS_BOOTS, HOOKSHOT)) # south of bridge
+        Location(5).add(DungeonChest(0x188), DungeonChest(0x18F)).connect(after_stalfos, AND(FEATHER, HOOKSHOT)) # small key, bridge chest
         stone_tablet = Location(5).add(DungeonChest(0x183)).connect(after_stalfos, AND(FEATHER, POWER_BRACELET))  # stone tablet
         Location(5).add(DungeonChest(0x186)).connect(after_stalfos, AND(FLIPPERS, HOOKSHOT))  # nightmare key
         boss = Location(5).connect(after_stalfos, AND(FEATHER, HOOKSHOT))
