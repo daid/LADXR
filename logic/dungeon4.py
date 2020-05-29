899dc02a82a3ab916a485c7737346b05d3f86b61
dragonc0 <66092539+dragonc0@users.noreply.github.com>
2020-05-29 21:28:10 +0200

    Logic changes to d4
    Add attack_powder for spike enemies
    include flippers to get past double lock as alternative to feather
    add feather to before_miniboss because of above change
    connect 2d water key to before_miniboss instead of after_double_lock
    add attack_hookshot to boss because of powder option at start

--- logic/dungeon4.py
+++ logic/dungeon4.py
@@ -8,15 +8,15 @@ class Dungeon4:
         entrance = Location(4)
         entrance.add(DungeonChest(0x179))  # stone slab chest
         entrance.add(DungeonChest(0x16A))  # map chest
-        right_of_entrance = Location(4).add(DungeonChest(0x178)).connect(entrance, AND(SHIELD, attack))
+        right_of_entrance = Location(4).add(DungeonChest(0x178)).connect(entrance, AND(SHIELD, attack_powder)) # 2 zol 1 spike enemy
         Location(4).add(DungeonChest(0x17B)).connect(right_of_entrance, SWORD) # room with key chest
         Location(4).add(DungeonChest(0x171)).add(DungeonChest(0x165)).connect(entrance, AND(FEATHER, PEGASUS_BOOTS, BOMB))  # 2 key chests on the right.
 
         double_locked_room = Location(4).connect(right_of_entrance, KEY4)
-        after_double_lock = Location(4).connect(double_locked_room, FEATHER)
+        after_double_lock = Location(4).connect(double_locked_room, OR(FEATHER, FLIPPERS))
         Location(4).add(DungeonChest(0x175)).connect(after_double_lock, FLIPPERS)
-        Location(4).add(DroppedKey(0x169)).connect(after_double_lock, FLIPPERS)  # key that drops in the hole and needs swim to get
-        before_miniboss = Location(4).connect(after_double_lock, AND(PEGASUS_BOOTS, KEY4))
+        before_miniboss = Location(4).connect(after_double_lock, AND(FEATHER, PEGASUS_BOOTS, KEY4))
+        Location(4).add(DroppedKey(0x169)).connect(before_miniboss, FLIPPERS)  # key that drops in the hole and needs swim to get
         before_miniboss.add(DungeonChest(0x16D))  # gel chest
         before_miniboss.add(DungeonChest(0x168))  # key chest near the puzzle
         miniboss = Location(4).connect(before_miniboss, AND(KEY4, POWER_BRACELET))
@@ -25,7 +25,7 @@ class Dungeon4:
         to_the_nightmare_key = Location(4).connect(before_miniboss, FLIPPERS)  # 5 symbol puzzle
         to_the_nightmare_key.add(DungeonChest(0x176))
 
-        boss = Location(4).connect(before_miniboss, AND(FLIPPERS, KEY4))
+        boss = Location(4).connect(before_miniboss, AND(attack_hookshot, FLIPPERS, KEY4))
 
         self.entrance = entrance
 
