17310e8006ae6590b2431b8d8d52f422c9ce9609
dragonc0 <66092539+dragonc0@users.noreply.github.com>
2020-05-29 22:47:58 +0200

    Logic changes for d7
    add new attack functions
    decouple pillars from area to allow exploration without power bracelet (ball room can be walked through
    more compass chest paths added (drop down and hit switch, or drop down and jump to ladder
    adjust logic further down for different layout
    put 100 rupee chest / medicine chest behind nightmare key

--- logic/dungeon7.py
+++ logic/dungeon7.py
@@ -6,19 +6,19 @@ from locations import *
 class Dungeon7:
     def __init__(self):
         entrance = Location(7)
-        Location(7).add(DroppedKey(0x210)).connect(entrance, attack)
-        post_first_door = Location(7).connect(entrance, KEY7)
-        topright_pillar = Location(7).add(DungeonChest(0x212)).connect(post_first_door, POWER_BRACELET)  # map chest
-        topright_pillar.add(DungeonChest(0x209))  # stone slab chest can be reached by dropping down a hole
-        bottomright_pillar = Location(7).add(DungeonChest(0x211)).connect(topright_pillar, FEATHER)  # compass chest
+        Location(7).add(DroppedKey(0x210)).connect(entrance, attack_powder)
+        topright_pillar_area = Location(7).connect(entrance, KEY7)
+        topright_pillar = Location(7).add(DungeonChest(0x212)).connect(topright_pillar_area, POWER_BRACELET)  # map chest
+        topright_pillar_area.add(DungeonChest(0x209))  # stone slab chest can be reached by dropping down a hole
+        bottomright_pillar = Location(7).add(DungeonChest(0x211)).connect(topright_pillar_area, OR(FEATHER, attack_hookshot), OR(SHIELD, BRACELET, attack_hookshot))  # compass chest; bracelet can be used in combination with ball; path without feather with hitting switch by falling on the raised blocks
         # Most of the dungeon can be accessed at this point.
-        bottomright_pillar.add(DroppedKey(0x21B))
-        bottomright_pillar.add(DungeonChest(0x224))  # nightmare key after the miniboss
-        bottomright_pillar.add(DungeonChest(0x21A))  # mirror shield chest
+        bottomleftF2_area = Location(7).add(DungeonChest(0x212)).connect(topright_pillar_area, attack_hookshot)  # area with hinox
+        bottomleftF2_area.add(DungeonChest(0x224))  # nightmare key after the miniboss
+        bottomleftF2_area.add(DungeonChest(0x21A))  # mirror shield chest
 
-        final_pillar = Location(7).add(DungeonChest(0x21C)).connect(bottomright_pillar, HOOKSHOT)  # chest that needs to spawn to get to the last pillar
-        final_pillar.add(DungeonChest(0x220))  # 100 rupee chest
+        final_pillar_area = Location(7).add(DungeonChest(0x21C)).connect(bottomleftF2_area, BOMB, HOOKSHOT)  # chest that needs to spawn to get to the last pillar
+        final_pillar = Location(7).connect(final_pillar_area, POWER_BRACELET) # decouple chest from pillar
 
-        boss = Location(7).connect(final_pillar, NIGHTMARE_KEY7)
+        boss = Location(7).add(DungeonChest(0x220)).connect(final_pillar, NIGHTMARE_KEY7) # 100 rupee chest / medicine chest (DX) behind boss door
 
         self.entrance = entrance
