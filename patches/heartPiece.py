990cba595ba6bdb97d834d7cb7a130357802f017
daid <daid303@gmail.com>
2020-06-01 23:56:59 +0200

    Fix holding piece of heart above link

--- patches/heartPiece.py
+++ patches/heartPiece.py
@@ -5,14 +5,13 @@ def fixHeartPiece(rom):
     # Patch all locations where the piece of heart is rendered.
     rom.patch(0x03, 0x1b52, ASM("ld de, $5A4D\ncall $3BC0"), ASM("ld a, $04\nrst 8"), fill_nop=True)  # state 0
 
-    # TODO: This does not show the entity above link.
-    rom.patch(0x03, 0x1A74, None, ASM("""
+    # Write custom code in the first state handler, this overwrites all state handlers
+    # Till state 5.
+    rom.patch(0x03, 0x1A74, 0x1A98, ASM("""
         ; Render sprite
         ld   a, $05
         rst  8
     
-        call $512A ; mark room as done
-        
         ; Handle item effect
         ld   a, $02
         rst  8
@@ -21,6 +20,23 @@ def fixHeartPiece(rom):
         ld   a, $03
         rst  8
         
+        ; Switch to state 5
+        ld   hl, $C290; stateTable
+        add  hl, bc
+        ld   [hl], $05
+        ret
+    """), fill_nop=True)
+    # Insert a state 5 handler
+    rom.patch(0x03, 0x1A98, 0x1B17, ASM("""
+        ; Render sprite
+        ld   a, $05
+        rst  8
+
+        ld   a, [$C19F] ; dialog state
+        and  a
+        ret  nz
+
+        call $512A ; mark room as done
         call $3F8D ; unload entity
         ret
-    """))
+    """), fill_nop=True)
