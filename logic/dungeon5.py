195a65d647b17fb5f29cdb32855dd8ccfe97b5e8
dragonc0 <66092539+dragonc0@users.noreply.github.com>
2020-06-21 17:34:29 +0200

    Logic changes d5
    Add path past gohma to logic
    hard: add kill mini-helmasaurs req to blade trap chest
    hard: add circumventing master stalfos via gohma pathway
    hard: boots to cross 2d bridge section
    hard: 2d section past gohma boots bonk across
    hard: cross pits north of crossroads with hookshot or boots bonk
    glitched: skip gohmas with hookshot clipping the pot

--- logic/dungeon5.py
+++ logic/dungeon5.py
@@ -16,20 +16,32 @@ class Dungeon5:
         Location(5).add(OwlStatue(0x19A)).connect(area2, STONE_BEAK5)
         Location(5).add(DungeonChest(0x19B)).connect(area2, attack_hookshot)  # map chest
         blade_trap_chest = Location(5).add(DungeonChest(0x197)).connect(area2, HOOKSHOT)  # key chest on the left
+        post_gohma = Location(5).connect(area2, HOOKSHOT, KEY5)
         after_stalfos = Location(5).add(DungeonChest(0x196)).connect(area2, AND(SWORD, BOMB)) # Need to defeat master stalfos once for this empty chest; l2 sword beams kill but obscure
-        Location(5).add(OwlStatue(0x18A)).connect(after_stalfos, AND(FEATHER, STONE_BEAK5))
-        Location(5).add(DungeonChest(0x18E)).connect(after_stalfos, AND(FEATHER, OR(PEGASUS_BOOTS, HOOKSHOT))) # south of bridge
-        Location(5).add(DungeonChest(0x188), DungeonChest(0x18F)).connect(after_stalfos, AND(FEATHER, HOOKSHOT)) # small key, bridge chest
-        stone_tablet = Location(5).add(DungeonChest(0x183)).connect(after_stalfos, AND(FEATHER, POWER_BRACELET))  # stone tablet
+        butterfly_owl = Location(5).add(OwlStatue(0x18A)).connect(after_stalfos, AND(FEATHER, STONE_BEAK5))
+        north_of_crossroads = Location(5).connect(after_stalfos, FEATHER)
+        Location(5).add(DungeonChest(0x18E)).connect(north_of_crossroads, OR(HOOKSHOT, AND(FEATHER, PEGASUS_BOOTS))) # south of bridge
+        other_bridge_chests = Location(5).add(DungeonChest(0x188), DungeonChest(0x18F)).connect(north_of_crossroads, HOOKSHOT) # small key, bridge chest
+        stone_tablet = Location(5).add(DungeonChest(0x183)).connect(north_of_crossroads, POWER_BRACELET)  # stone tablet
         boss_key = Location(5).add(DungeonChest(0x186)).connect(after_stalfos, AND(FLIPPERS, HOOKSHOT))  # nightmare key
         before_boss = Location(5).connect(after_stalfos, AND(FEATHER, HOOKSHOT))
+        before_boss.connect(post_gohma, AND(FEATHER, HOOKSHOT, KEY5, FOUND(KEY5, 3))) # path via gohma
         boss = Location(5).add(HeartContainer(0x185)).connect(before_boss, AND(HOOKSHOT, SWORD, NIGHTMARE_KEY5))
 
         # When we can reach the stone tablet chest, we can also reach the final location of master stalfos
         stone_tablet.add(HookshotDrop())
 
         if options.logic == 'hard' or options.logic == 'glitched':
-            blade_trap_chest.connect(area2, FEATHER)
-            boss_key.connect(after_stalfos, AND(FEATHER, PEGASUS_BOOTS))
-
+            blade_trap_chest.connect(area2, FEATHER, attack_hookshot_powder)
+            boss_key.connect(after_stalfos, AND(FEATHER, PEGASUS_BOOTS)) # boots jump across
+            after_stalfos.connect(post_gohma, AND(FEATHER, HOOKSHOT, KEY5, FOUND(KEY5, 3))) # passage past gohma
+            butterfly_owl.connect(post_gohma, AND(FEATHER, HOOKSHOT, KEY5, FOUND(KEY5, 3))) # passage past gohma
+            butterfly_owl.connect(after_stalfos, PEGASUS_BOOTS) # boots charge + bonk to cross 2d bridge
+            before_boss.connect(post_gohma, AND(PEGASUS_BOOTS, HOOKSHOT)) # boots bonk in 2d section to skip feather
+            north_of_crossroads.connect(after_stalfos, OR(HOOKSHOT, PEGASUS_BOOTS) # boots bonk over the pits, or hookshot the right block
+            
+        if options.logic == 'glitched':
+            post_gohma.connect(area2, HOOKSHOT) # glitch through the blocks/pots with hookshot. Zoomerang can be used but has no logical implications because of 2d section requiring hookshot
+            #after_stalfos.connect(post_gohma, AND(FEATHER, HOOKSHOT, OR(SWORD, BOW, MAGIC_ROD))) # TODO: requires nag messages from key block before boss
+            
         self.entrance = entrance
