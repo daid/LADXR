8342027a72c305d76dc18e35da877a3498bf998e
daid <daid303@gmail.com>
2020-06-24 20:00:32 +0200

    Fix crash when disabling heartpiece shuffle

--- locations/beachSword.py
+++ locations/beachSword.py
@@ -1,10 +1,10 @@
-from .heartPiece import HeartPiece
+from .droppedKey import DroppedKey
 from .items import *
 from roomEditor import RoomEditor
 from assembler import ASM
 
 
-class BeachSword(HeartPiece):
+class BeachSword(DroppedKey):
     def __init__(self):
         super().__init__(0x0F2)
 
@@ -13,7 +13,7 @@ class BeachSword(HeartPiece):
             # Set the heart piece data
             super().patch(rom, option, cross_world=cross_world)
 
-            # Patch the room to contain a chest instead of the sword on the beach
+            # Patch the room to contain a heart piece instead of the sword on the beach
             re = RoomEditor(rom, 0x0F2)
             re.removeEntities(0x31)  # remove sword
             re.addEntity(5, 5, 0x35)  # add heart piece
@@ -22,7 +22,6 @@ class BeachSword(HeartPiece):
             # Prevent shield drops from the like-like from turning into swords.
             rom.patch(0x03, 0x1B9C, ASM("ld a, [$DB4E]"), ASM("ld a, $01"), fill_nop=True)
             rom.patch(0x03, 0x244D, ASM("ld a, [$DB4E]"), ASM("ld a, $01"), fill_nop=True)
-
 
     def read(self, rom):
         re = RoomEditor(rom, 0x0F2)
