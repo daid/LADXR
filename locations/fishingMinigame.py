e24b1681b81cbee749a127d5e7c23d7bcdd12d88
Daid <daid303@gmail.com>
2021-02-22 23:33:34 +0100

    Fix the fishing minigame being randomized if heartpieces are not enabled

--- locations/fishingMinigame.py
+++ locations/fishingMinigame.py
@@ -7,3 +7,9 @@ class FishingMinigame(DroppedKey):
 
     def __init__(self):
         super().__init__(0x2B1)
+
+    def configure(self, options):
+        if options.heartpiece:
+            super().configure(options)
+        else:
+            self.OPTIONS = [HEART_PIECE]
