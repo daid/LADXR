bd5f55eca507ad8f6e397a04bbf7aa313f02fc32
Daid <daid303@gmail.com>
2021-02-23 21:34:47 +0100

    fix when the fishing minigame is ruppees they get stolen from you.

--- patches/fishingMinigame.py
+++ patches/fishingMinigame.py
@@ -10,4 +10,10 @@ def updateFinishingMinigame(rom):
         ; Mark selection as stopping minigame, as we are not asking a question.
         ld   a, $01
         ld   [$C177], a
+        
+        ; Check if we got rupees from the item skip getting rupees from the fish.
+        ld   a, [$DB90]
+        ld   hl, $DB8F
+        or   [hl]
+        jp   nz, $66FE
     """), fill_nop=True)
