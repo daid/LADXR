59fd9a5783fe91d17f2af2637fa20318a915e75c
David Braam <david.braam@sioux.eu>
2021-03-03 13:53:31 +0100

    Fix that return-to-home will allow you to steal from the shop

--- patches/core.py
+++ patches/core.py
@@ -140,6 +140,10 @@ noWrapDown:
         and  a
         jp   z, $40BE ; return to normal "return to game" handling
 
+        ld   a, [$C509] ; Check if we have an item in the shop
+        and  a
+        jp   z, $40BE ; return to normal "return to game" handling
+
         ld   a, $0B
         ld   [$DB95], a
         call $0C7D
