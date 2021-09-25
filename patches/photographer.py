cc2124a721f3f77c33cc3cfc43f38564ed2b8878
Daid <daid303@gmail.com>
2021-09-25 18:20:19 +0200

    Properly fix richard photo

--- patches/photographer.py
+++ patches/photographer.py
@@ -5,7 +5,7 @@ def fixPhotographer(rom):
     rom.patch(0x36, 0x3234, ASM("jr nz, $52"), "", fill_nop=True)
     rom.patch(0x36, 0x3240, ASM("jr z, $46"), "", fill_nop=True)
     # Allow richard photo when castle is opened
-    rom.patch(0x36, 0x71FF, ASM("jp nz, $7288"), "", fill_nop=True)
+    rom.patch(0x36, 0x31FF, ASM("jp nz, $7288"), "", fill_nop=True)
     # Allow photographer with bowwow saved
     rom.patch(0x36, 0x0398, ASM("or [hl]"), "", fill_nop=True)
     rom.patch(0x36, 0x3180, ASM("or [hl]"), "", fill_nop=True)
