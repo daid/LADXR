e0286ec05ff3c859749a0c65c9f74fcfd96527a2
Daid <daid303@gmail.com>
2021-09-25 16:26:47 +0200

    Allow richard photo with castle open

--- patches/photographer.py
+++ patches/photographer.py
@@ -4,6 +4,8 @@ def fixPhotographer(rom):
     # Allow richard photo without slime key
     rom.patch(0x36, 0x3234, ASM("jr nz, $52"), "", fill_nop=True)
     rom.patch(0x36, 0x3240, ASM("jr z, $46"), "", fill_nop=True)
+    # Allow richard photo when castle is opened
+    rom.patch(0x36, 0x71FF, ASM("jp nz, $7288"), "", fill_nop=True)
     # Allow photographer with bowwow saved
     rom.patch(0x36, 0x0398, ASM("or [hl]"), "", fill_nop=True)
     rom.patch(0x36, 0x3180, ASM("or [hl]"), "", fill_nop=True)
