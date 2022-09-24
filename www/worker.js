f26a8afa58eec79594e8d99e73755d8959ef6371
Daid <daid303@gmail.com>
2022-09-24 18:51:27 +0000

    Small tweak to the wasm worker

--- www/worker.js
+++ www/worker.js
@@ -12,20 +12,19 @@ let pyodideReadyPromise = loadPyodideAndPackages();
 self.onmessage = async (event) => {
     await pyodideReadyPromise;
     try {
-        console.log(event.data);
         self.pyodide.FS.writeFile("/input.gbc", event.data["input.gbc"]);
         self.pyodide.FS.writeFile("/spoiler.txt", "");
         self.pyodide.runPython("import sys;import io;sys.stdout = io.StringIO();");
         self.pyladxr.main(event.data["args"]);
-        var stdout = await self.pyodide.runPythonAsync("sys.stdout.getvalue()");
+        var stdout = self.pyodide.runPython("sys.stdout.getvalue()");
         var seed = "???";
         for(var line of stdout.split("\n")) {
             if (line.startsWith("Seed:"))
                 seed = line.substr(5).trim();
         }
 
-        self.postMessage({"success": true, "seed": seed, "romFilename": "LADXR_" + seed + ".gbc", "rom": pyodide.FS.readFile("/output.gbc"), "spoiler": pyodide.FS.readFile("/spoiler.txt", {"encoding": "utf8"})});
+        self.postMessage({"id": event.data.id, "success": true, "seed": seed, "romFilename": "LADXR_" + seed + ".gbc", "rom": pyodide.FS.readFile("/output.gbc"), "spoiler": pyodide.FS.readFile("/spoiler.txt", {"encoding": "utf8"})});
     } catch (error) {
-        self.postMessage({"success": false, "message": error.message});
+        self.postMessage({"id": event.data.id, "success": false, "message": error.message});
     }
 };
