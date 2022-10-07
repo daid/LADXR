9bf6198bdc36b58b95e97a20690815dac7594128
Daid <daid303@gmail.com>
2022-10-07 11:15:49 +0000

    Add multiworld client side generator

--- www/js/worker.js
+++ www/js/worker.js
@@ -24,7 +24,7 @@ self.onmessage = async (event) => {
         self.pyodide.FS.writeFile("/input.gbc", event.data["input.gbc"]);
         self.pyodide.FS.writeFile("/spoiler.txt", "");
         stdout = [];
-        console.log("Started randomizer")
+        console.log("Started randomizer:", event.data.args)
         self.pyladxr.main(["/input.gbc", "--output", "/output.gbc", "--spoilerfilename", "/spoiler.txt"].concat(event.data["args"]));
         var seed = "???";
         for(var line of stdout) {
@@ -32,7 +32,10 @@ self.onmessage = async (event) => {
                 seed = line.substr(5).trim();
         }
         console.log(`Randomizer finished for seed: ${seed}`)
-        self.postMessage({type: "done", id: event.data.id, success: true, seed: seed, romFilename: "LADXR_" + seed + ".gbc", rom: pyodide.FS.readFile("/output.gbc"), spoiler: pyodide.FS.readFile("/spoiler.txt", {"encoding": "utf8"})});
+        var ext = ".gbc";
+        if (event.data.args.indexOf("--multiworld") > 0)
+            ext = ".zip";
+        self.postMessage({type: "done", id: event.data.id, success: true, seed: seed, romFilename: "LADXR_" + seed + ext, rom: pyodide.FS.readFile("/output.gbc"), spoiler: pyodide.FS.readFile("/spoiler.txt", {"encoding": "utf8"})});
     } catch (error) {
         self.postMessage({type: "done", id: event.data.id, success: false, message: error.message});
     }
