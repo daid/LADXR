e9815ff70e6404c2926df9f10c2b462167357fcc
dragonc0 <66092539+dragonc0@users.noreply.github.com>
2022-10-14 14:27:35 +0200

    dungeon 7 switch issues fix
    force attack_hookshot requirement for chests from bottomleftF2_area where they were implied
    
    glitch logic: seperate topleftF2_area (tile room) from bottomleftF2_area (hinox room) so superjumps without hitting switch are accounted for.
    -add switchless path to bottom left hinox with boots + feather
    -add ball for south 3 of a kind room for kill method as it makes bottom left pillar less of a hassle in glitch logic

--- logic/dungeon7.py
+++ logic/dungeon7.py
@@ -15,14 +15,17 @@ class Dungeon7:
             Location(7).add(OwlStatue(0x204)).connect(topright_pillar_area, STONE_BEAK7)
         topright_pillar_area.add(DungeonChest(0x209))  # stone slab chest can be reached by dropping down a hole
         three_of_a_kind_north = Location(7).add(DungeonChest(0x211)).connect(topright_pillar_area, OR(r.attack_hookshot, AND(FEATHER, SHIELD)))  # compass chest; path without feather with hitting switch by falling on the raised blocks. No bracelet because ball does not reset
-        bottomleftF2_area = Location(7).add(DungeonChest(0x201)).connect(topright_pillar_area, r.attack_hookshot)  # area with hinox
+        bottomleftF2_area = Location(7).connect(topright_pillar_area, r.attack_hookshot)  # area with hinox, be able to hit a switch to reach that area
+        topleftF1_chest = Location(7).add(DungeonChest(0x201)) # top left chest on F1
+        bottomleftF2_area.connect(topleftF1_chest, None, one_way = True)  # drop down in left most holes of hinox room or tile room
         Location(7).add(DroppedKey(0x21B)).connect(bottomleftF2_area, r.attack_hookshot) # hinox drop key
         # Most of the dungeon can be accessed at this point.
         if options.owlstatues == "both" or options.owlstatues == "dungeon":
             bottomleft_owl = Location(7).add(OwlStatue(0x21C)).connect(bottomleftF2_area, AND(BOMB, STONE_BEAK7))
         nightmare_key = Location(7).add(DungeonChest(0x224)).connect(bottomleftF2_area, r.miniboss_requirements[world_setup.miniboss_mapping[6]]) # nightmare key after the miniboss
-        bottomleftF2_area.add(DungeonChest(0x21A))  # mirror shield chest
-        toprightF1_chest = Location(7).add(DungeonChest(0x204)).connect(bottomleftF2_area, r.attack_hookshot)  # chest on the F1 right ledge. Added attack_hookshot since 1 requirement is needed
+        mirror_shield_chest = Location(7).add(DungeonChest(0x21A)).connect(bottomleftF2_area, r.attack_hookshot)  # mirror shield chest, need to be able to hit a switch to reach or
+        bottomleftF2_area.connect(mirror_shield_chest, AND(KEY7, FOUND(KEY7, 3)), one_way = True) # reach mirror shield chest from hinox area by opening keyblock
+        toprightF1_chest = Location(7).add(DungeonChest(0x204)).connect(bottomleftF2_area, r.attack_hookshot)  # chest on the F1 right ledge. Added attack_hookshot since switch needs to be hit to get back up
         final_pillar_area = Location(7).add(DungeonChest(0x21C)).connect(bottomleftF2_area, AND(BOMB, HOOKSHOT))  # chest that needs to spawn to get to the last pillar
         final_pillar = Location(7).connect(final_pillar_area, POWER_BRACELET) # decouple chest from pillar
 
@@ -37,10 +40,12 @@ class Dungeon7:
         if options.logic == 'glitched' or options.logic == 'hell':
             topright_pillar_area.connect(entrance, AND(FEATHER, SWORD)) # superjump in the center to get on raised blocks, superjump in switch room to right side to walk down. center superjump has to be low so sword added
             toprightF1_chest.connect(topright_pillar_area, FEATHER) # superjump from F1 switch room
-            topright_pillar_area.connect(bottomleftF2_area, FEATHER) # superjump in top left pillar room over the blocks from right to left
-            final_pillar_area.connect(bottomleftF2_area, r.attack_hookshot) # sideways block push to get to the chest and pillar
+            topleftF2_area = Location(7).connect(topright_pillar_area, FEATHER) # superjump in top left pillar room over the blocks from right to left, to reach tile room
+            topleftF2_area.connect(topleftF1_chest, None, one_way = True) # fall down tile room holes on left side to reach top left chest on ground floor
+            topleftF1_chest.connect(bottomleftF2_area, AND(PEGASUS_BOOTS, FEATHER), one_way = True) # without hitting the switch, jump on raised blocks at f1 pegs chest (0x209), and boots jump to stairs to reach hinox area
+            final_pillar_area.connect(bottomleftF2_area, OR(r.attack_hookshot, POWER_BRACELET, AND(FEATHER, SHIELD))) # sideways block push to get to the chest and pillar, kill requirement for 3 of a kind enemies to access chest. Assumes you do not get ball stuck on raised pegs for bracelet path
             if options.owlstatues == "both" or options.owlstatues == "dungeon":
-                bottomleft_owl.connect(bottomleftF2_area, STONE_BEAK7) # sideways block push to get to the owl statue (attack_hookshot is already implied from bottomleftF2_area)
+                bottomleft_owl.connect(bottomleftF2_area, STONE_BEAK7) # sideways block push to get to the owl statue
             final_pillar.connect(bottomleftF2_area, BOMB) # bomb trigger pillar
             pre_boss.connect(final_pillar, FEATHER) # superjump on top of goomba to extend superjump to boss door plateau
             pre_boss.connect(beamos_horseheads_area, None, one_way=True) # can drop down from raised plateau to beamos horseheads area
