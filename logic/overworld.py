c7dbdea654060861e99358662b93c780ed962a7b
kbranch <kbranch@kbranch.net>
2023-03-20 17:53:34 -0400

    Removed the D7 exception, added an exit to logic (#103)

--- logic/overworld.py
+++ logic/overworld.py
@@ -413,6 +413,7 @@ class World:
         d7_platau = Location()
         d7_tower = Location()
         d7_platau.connect(d7_tower, AND(POWER_BRACELET, BIRD_KEY), one_way=True)
+        d7_tower.connect(d7_platau, None, one_way=True)
         self._addEntrance("right_taltal_connector1", water_cave_hole, right_taltal_connector1, None)
         self._addEntrance("right_taltal_connector2", right_taltal_connector_outside1, right_taltal_connector1, None)
         self._addEntrance("right_taltal_connector3", right_taltal_connector_outside1, right_taltal_connector2, None)
