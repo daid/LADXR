c81340f1803c098b692589684c83f4b53a2ec7bd
Daid <daid303@gmail.com>
2026-01-16 16:44:33 +0100

    Some more music export stuff...

--- musicData.py
+++ musicData.py
@@ -1,14 +1,30 @@
 import struct
+import binascii
 
 
 OP_END = 0x00
 OP_REST = 0x01
 OP_LOOP_START = 0x9B
 OP_LOOP_END = 0x9C
-OP_CHANNEL3_SET_WAVEFORM = 0x9D
-OP_CHANNEL12_SET_INSTRUMENT = 0x9D
+OP_ENABLE_UNKNOWN1 = 0x95
+OP_DISABLE_UNKNOWN1 = 0x96
+OP_ENABLE_UNKNOWN2 = 0x97
+OP_DISABLE_UNKNOWN2 = 0x98
+OP_ENABLE_UNKNOWN3 = 0x99
+OP_DISABLE_UNKNOWN3 = 0x9A
+OP_SET_INSTRUMENT = 0x9D
 OP_SET_SPEED_DATA = 0x9E
 OP_SET_TRANSPOSE = 0x9F
+
+
+def noteToString(channel_idx, note):
+    if note is None:
+        return "   "
+    assert 2 <= note <= 0x90
+    assert (note & 1) == 0
+    octave = note // 24
+    note = ["C_",  "C#", "D_", "D#", "E_", "F_", "F#", "G_", "G#", "A_", "A#", "B_"][(note // 2) % 12]
+    return f"{note}{octave+1}"
 
 
 class SongBlock:
@@ -17,7 +33,7 @@ class SongBlock:
     
     def oopsAllRest(self):
         for op in self.ops:
-            if op[0] not in {OP_REST, OP_LOOP_START, OP_LOOP_END, OP_CHANNEL12_SET_INSTRUMENT} and (op[0] < 0xA0 or op[0] > 0xAF):
+            if op[0] not in {OP_REST, OP_LOOP_START, OP_LOOP_END, OP_SET_INSTRUMENT} and (op[0] < 0xA0 or op[0] > 0xAF):
                 return False
         return True
 
@@ -74,7 +90,7 @@ class SongPlayback:
                     self.delay[channel_idx] = self.speed[channel_idx]
                 elif 0xA0 <= op[0] <= 0xAF: # notlen
                     self.speed[channel_idx] = self.speed_data[op[0] & 0x0F]
-                elif op[0] == OP_CHANNEL12_SET_INSTRUMENT:
+                elif op[0] == OP_SET_INSTRUMENT:
                     pass
                 elif op[0] == OP_SET_SPEED_DATA:
                     self.speed_data = op[1]
@@ -103,35 +119,81 @@ class SongPlayback:
 class LADXMExporter:
     def __init__(self, song: Song, filename: str):
         sp = SongPlayback(song)
-        timestep = 0
-        export_block = [[]]
+        pattern = [[]]
+        pulse_instruments = []
+        wave_instruments = []
         while not sp.isFinished():
             def f(channel_idx, op):
-                export_block[-1].append((channel_idx, op))
-            timestep += sp.step(f)
+                if op[0] == OP_SET_INSTRUMENT:
+                    if channel_idx == 2:
+                        if op not in wave_instruments:
+                            wave_instruments.append(op)
+                    elif op not in pulse_instruments:
+                        pulse_instruments.append(op)
+                pattern[-1].append((channel_idx, op))
+            timestep = sp.step(f)
             for n in range(timestep):
-                export_block.append([])
-        export_block.pop()
+                pattern.append([])
+        pattern.pop()
 
         f = open(filename, "wt")
         f.write("version: 1\n")
-        f.write("speed: 1\n")
+        current_speed = 1
+        f.write(f"speed: {current_speed}\n")
+        for instrument_index, instrument in enumerate(pulse_instruments):
+            length = 0x3F - instrument[3] & 0x3F
+            duty = instrument[3] >> 6
+            volume = instrument[1] >> 4
+            env_dir_inc = "-" if (instrument[1] & 0x08) == 0 else "+"
+            env_pace = instrument[1] & 0x07
+            f.write(f"pulse_instrument{instrument_index}: length: {length} duty: {duty} volume: {volume} vol_change: {env_dir_inc}{env_pace} unk1: {instrument[2]}\n")
+        for instrument_index, instrument in enumerate(wave_instruments):
+            f.write(f"wave_instrument{instrument_index}: {binascii.hexlify(instrument[2]).decode('ascii')} unk1: {instrument[1]}\n")
         f.write("sequence:\n")
         f.write("  intro\n")
         f.write("  main\n")
-        f.write("pattern: into\n")
+        f.write("pattern: intro\n")
         f.write("pattern: main\n")
-        for block in export_block:
+        pattern_index = 0
+        while pattern_index < len(pattern):
             notes = [None, None, None, None]
             effects = [[], [], [], []]
-            for channel_idx, op in block:
-                if 0x02 <= op[0] <= 0x90 or (op[0] == 0x01 and channel_idx == 3): # A note to play
-                    notes[channel_idx] = op[0]
-                elif op[0] == 0x01:
-                    pass # Ignore rests
-                else:
-                    effects[channel_idx].append(op)
-            print(notes, effects)
+            for n in range(0, current_speed):
+                if pattern_index + n >= len(pattern):
+                    continue
+                block = pattern[pattern_index + n]
+                if n != 0 and any(block):
+                    print(f"Warning slight time shift at {pattern_index//current_speed} -{n} frames")
+                for channel_idx, op in block:
+                    if 0x02 <= op[0] <= 0x90 or (op[0] == 0x01 and channel_idx == 3): # A note to play
+                        if notes[channel_idx] is not None:
+                            print(f"Major warning, note erased at {pattern_index//current_speed}")
+                        notes[channel_idx] = op[0]
+                    elif op[0] == 0x01:
+                        pass # Ignore rests
+                    else:
+                        effects[channel_idx].append(op)
+            if (pattern_index // current_speed) % 8 == 0:
+                f.write("= ")
+            else:
+                f.write("- ")
+            pattern_index += current_speed
+            for channel_idx in range(4):
+                f.write(noteToString(channel_idx, notes[channel_idx]))
+                for effect in effects[channel_idx]:
+                    if effect[0] == OP_SET_INSTRUMENT:
+                        if channel_idx == 2:
+                            f.write(f" I{wave_instruments.index(effect):02}")
+                        else:
+                            f.write(f" I{pulse_instruments.index(effect):02}")
+                    elif effect[0] == OP_ENABLE_UNKNOWN3:
+                        f.write(f" EU3")
+                    elif effect[0] == OP_DISABLE_UNKNOWN3:
+                        f.write(f" DU3")
+                    else:
+                        raise NotImplementedError(hex(effect[0]))
+                f.write("    " * (4 - len(effects[channel_idx])))
+            f.write("\n")
         f.close()
 
 
@@ -217,14 +279,14 @@ class MusicData:
             if self._rb[addr-0x4000] == OP_LOOP_START: # Loop
                 addr += 1
                 block.ops.append((OP_LOOP_START, self._rb[addr-0x4000]))
-            elif self._rb[addr-0x4000] == OP_CHANNEL3_SET_WAVEFORM and channel_type == 3: # Set waveform (channel 3)
+            elif self._rb[addr-0x4000] == OP_SET_INSTRUMENT and channel_type == 3: # Set waveform (channel 3)
                 wave_ptr = self._get_ptr(addr + 1)
                 wave_data = self._get_block(wave_ptr, 16)
                 addr += 3
-                block.ops.append((OP_CHANNEL3_SET_WAVEFORM, self._rb[addr-0x4000], wave_data))
-            elif self._rb[addr-0x4000] == OP_CHANNEL12_SET_INSTRUMENT: # Set envelope duty (channel 1/2)
+                block.ops.append((OP_SET_INSTRUMENT, self._rb[addr-0x4000], wave_data))
+            elif self._rb[addr-0x4000] == OP_SET_INSTRUMENT: # Set envelope duty (channel 1/2)
                 addr += 3
-                block.ops.append((OP_CHANNEL12_SET_INSTRUMENT, self._rb[addr-0x4000-2], self._rb[addr-0x4000-1], self._rb[addr-0x4000]))
+                block.ops.append((OP_SET_INSTRUMENT, self._rb[addr-0x4000-2], self._rb[addr-0x4000-1], self._rb[addr-0x4000]))
             elif self._rb[addr-0x4000] == OP_SET_SPEED_DATA: # Set speed
                 speed_ptr = self._get_ptr(addr + 1)
                 speed_data = self._get_block(speed_ptr, 16)
@@ -266,9 +328,9 @@ class MusicData:
             for block in channel.blocks:
                 bin_block = bytearray()
                 for op in block.ops:
-                    if op[0] == OP_CHANNEL3_SET_WAVEFORM and channel_type == 3: # Waveform special case which has a pointer
+                    if op[0] == OP_SET_INSTRUMENT and channel_type == 3: # Waveform special case which has a pointer
                         addr = store_block(op[2])
-                        bin_block += struct.pack("<BHB", OP_CHANNEL3_SET_WAVEFORM, addr, op[1])
+                        bin_block += struct.pack("<BHB", OP_SET_INSTRUMENT, addr, op[1])
                     elif op[0] == OP_SET_SPEED_DATA:
                         addr = store_block(op[1])
                         bin_block += struct.pack("<BH", OP_SET_SPEED_DATA, addr)
@@ -318,7 +380,7 @@ if __name__ == "__main__":
     a.store(r)
     b.store(r)
 
-    song_idx = 3
+    song_idx = 4
     LADXMExporter(a.songs[song_idx], f"music/a_{song_idx:02}.ladxm")
     exit(0)
 
